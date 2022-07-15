0fada8edc048203c563847a598d804a5ef0c7d8c
Дмитрий Бунецкий <bunetskydn@gmail.com>
2022-07-16 00:24:42 +0700

    feat: add style files and fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 import { defineNuxtConfig } from 'nuxt';
+import { resolve } from 'path';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({});
+export default defineNuxtConfig({
+  css: ['@/assets/style/_main.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/assets/style/cfg-scss";`,
+        },
+      },
+    },
+  },
+});
