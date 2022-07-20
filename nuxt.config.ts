190c6562b6049986c878415accdf62a53e17213d
Дмитрий Бунецкий <bunetskydn@gmail.com>
2022-07-21 00:06:37 +0700

    feat: update add dependencies and set up project config files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,4 @@
 import { defineNuxtConfig } from 'nuxt';
-import { resolve } from 'path';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
@@ -12,5 +11,22 @@ export default defineNuxtConfig({
         },
       },
     },
+  },
+  modules: ['@pinia/nuxt'],
+  head: {
+    title: 'MyCV',
+    htmlAttrs: {
+      lang: 'ru',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+    link: [
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+    ],
   },
 });
