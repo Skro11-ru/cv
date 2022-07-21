b44e3cacf0b3c231ed331b2c39c42b941194c6ec
Дмитрий Бунецкий <bunetskydn@gmail.com>
2022-07-21 14:45:21 +0700

    fix: fix broken image links after build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ import { defineNuxtConfig } from 'nuxt';
 export default defineNuxtConfig({
   css: ['@/assets/style/_main.scss'],
   vite: {
+    base: '/',
     css: {
       preprocessorOptions: {
         scss: {
